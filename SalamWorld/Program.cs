0c223f5a1c0a1379f91b4b58274bf5c5db92eb77
iAlhasan <Prowess@live.com>
2019-01-09 09:03:31 -0500

    Add project files.

---
+++ SalamWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalamWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
